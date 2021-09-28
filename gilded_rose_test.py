b9596dbe342b0a47b0f8cedeac46752350e7ea70
Pascal Clarkson <info@artocraft.nl>
2021-09-28 15:18:15 +0000

    Updated approval test

--- gilded_rose_test.py
+++ gilded_rose_test.py
@@ -1,29 +1,26 @@
 import unittest
-from approvaltests.approvals import verify
+from approvaltests import combination_approvals
 from gilded_rose import Item, GildedRose
 
+def doUpdateItem(name: str, sell_in: int, quality: int):
+    item = Item(name, sell_in, quality)
+    app = GildedRose([item])
+    app.update_quality()
+    return str(app.items[0])
+
+
 class GildedRoseTest(unittest.TestCase):
-  def test_main(self):
-      items = [
-        Item(name='+5 Dexterity Vest', sell_in=10, quality=20),
-        Item(name='Aged Brie', sell_in=2, quality=0),
-        Item(name='Elixir of the Mongoose', sell_in=5, quality=7),
-        Item(name='Sulfuras, Hand of Ragnaros', sell_in=0, quality=80),
-        Item(name='Sulfuras, Hand of Ragnaros', sell_in=-1, quality=80),
-        Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=15, quality=20),
-        Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=10, quality=49),
-        Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=5, quality=49),
-        Item(name='Conjured Mana Cake', sell_in=3, quality=6),  # <-- :O
-      ]
+    def test_main(self):
+        combination_approvals.verify_all_combinations(doUpdateItem, [[
+            'foo', 'Aged Brie', 'Backstage passes to a TAFKAL80ETC concert',
+            'Sulfuras, Hand of Ragnaros'
+        ], [-1, 2, 0, 6, 11], [0, 1, 49, 50]])
 
-      days = 30
-      for day in range(days + 1):
-        GildedRose(items).update_quality()      
 
-      #GildedRose(items).update_quality()
-      
-      verify(items)
+# Mutation testing is a good thing to do as well!
+# By doing manual mutations in the production code 
+# we can check if our test set is strong enough.
+# https://leanpub.com/b/codekatas
 
 if __name__ == "__main__":
     unittest.main()
-
