5641514978e9b7c2c74f520689b699bd4dba0b86
Pascal Clarkson <info@artocraft.nl>
2022-06-23 11:01:42 +0000

    Old old changes

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-poetry run pytest && poetry run coverage run --branch --source gilded_rose gilded_rose_test.py && poetry run coverage report -m && poetry run coverage html
+poetry run pytest && \
+	poetry run coverage erase && \
+	poetry run coverage run --branch --source gilded_rose gilded_rose_test.py && \
+	poetry run coverage report -m && \
+	poetry run coverage html
